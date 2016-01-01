2dd1e94cfec538b514f9013267591e161380da9e
BeanJr <kyle95wm@gmail.com>
2016-01-01 16:35:21 -0500

    More possible fixes for ports

--- ufw-helper.sh
+++ ufw-helper.sh
@@ -2,6 +2,8 @@
 
 # This script is designed to help you activate UFW and open all the appropriate ports
 # for your server.
+# All ports will be opened on both TCP and UDP since I'm unsure which ones are really TCP and UDP
+# This is also a BETA script so things might not work!!
 if [ $UID != 0 ] ; then
 	You must be root!
 fi
@@ -17,40 +19,19 @@ if [ "$1" == "install" ] ; then
 	if [ $ssh == y ] ; then
 		ufw allow ssh
 	fi
-	ufw allow 8000/tcp
-	ufw allow 9000:9002/tcp
-	read -p "Want to allow the admin page through the firewall? [y/n] " admin
-	if [ $admin == y ] ; then
-		ufw allow 9009/tcp
-	fi
+	ufw allow 8000,9000,9001,9002,9009,53,27500,27900,27901,28910,29900,29901,29920/tcp
+	ufw allow 8000,9000,9001,9002,9009,53,27500,27900,27901,28910,29900,29901,29920/udp
 	read -p "Allow console registration page to be accessed? [y/n] " reg
 	if [ $reg == y ] ; then
-		ufw allow 9998
+		ufw allow 9998/tcp
+		ufw allow 9998/udp
 	fi
-	ufw allow 53/tcp
-	ufw allow 27500/tcp
-	ufw allow 27900/tcp
-	ufw allow 27901/tcp
-	ufw allow 28910/tcp
-	ufw allow 29900/tcp
-	ufw allow 29901/tcp
-	ufw allow 29920/tcp
 	echo "Rules added"
 	ufw enable
 elif [ "$1" == "uninstall" ] ; then
-	ufw delete allow 53/tcp
-	ufw delete allow 8000/tcp
-	ufw delete allow 9000:9002/tcp
-	ufw delete allow 9009/tcp
-	ufw delete allow 9998
-	ufw delete allow 27500/tcp
-	ufw delete allow 27900/tcp
-	ufw delete allow 27901/tcp
-	ufw delete allow 28910/tcp
-	ufw delete allow 29900/tcp
-	ufw delete allow 29901/tcp
-	ufw delete allow 29920/tcp
-	echo "Rules deleted. Now disabling firewall (you can re-enable if you want)"
+	ufw delete allow 8000,9000,9001,9002,9009,53,27500,27900,27901,28910,29900,29901,29920/tcp
+	ufw delete allow 8000,9000,9001,9002,9009,53,27500,27900,27901,28910,29900,29901,29920/udp
+	echo "ALTWFC Rules deleted. Now disabling firewall (you can re-enable if you want)"
 	ufw disable
 else
 	echo "Either type install or uninstall after the name of the script."
